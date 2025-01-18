5d19c3c522484ef31da10fe52570bbc00d6d526e
viruseg <virus85@yandex.ru>
2025-01-18 15:43:25 +0300

    Disabled automatic appearance of menus. Fixed styles.

--- dist/main.user.js
+++ dist/main.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Interface fixes on myshows.me
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.11
 // @description  Fixing interface styles on myshows.me
 // @author       viruseg
 // @match        *.myshows.me/*
 // @run-at       document-start
 // @grant        GM_addStyle
 // @icon         https://myshows.me/favicon-32x32.png
-// @author       viruseg
 // @homepageURL  https://github.com/viruseg/MyShowsInterfaceFix
 // @updateURL    https://raw.githubusercontent.com/viruseg/MyShowsInterfaceFix/master/dist/main.js
 // @downloadURL  https://raw.githubusercontent.com/viruseg/MyShowsInterfaceFix/master/dist/main.js
@@ -17,93 +16,134 @@
 
 GM_addStyle(
 `
-.Unwatched-showTitle-title{
-  font-weight: 500!important;
+.Unwatched-showTitle-title
+{
+    font-weight: 500 !important;
 }
 
-.Unwatched-season~div .UnwatchedEpisodeItem, .RowEpisodeBySeason > .Row-container > div {
-	padding-top: 5px!important;
-	padding-bottom: 5px!important;
+.Unwatched-season ~ div .UnwatchedEpisodeItem, .RowEpisodeBySeason > .Row-container > div
+{
+    padding-top: 5px !important;
+    padding-bottom: 5px !important;
 }
 
-.RowEpisodeBySeason > .Row-container {
-	height: 30px;
+.RowEpisodeBySeason > .Row-container
+{
+    height: 30px !important;
 }
 
-.Comment__actions {
+.Comment__actions
+{
     border-bottom: 1px solid var(--border-gray-color) !important;
 }
 
-.EpisodeWatchLabel {
-  width: 13px!important;
-  height: 13px!important;
+.EpisodeWatchLabel
+{
+    width: 13px !important;
+    height: 13px !important;
 }
 
 .episodes-by-season__season .MyLabel.corner,
 .Unwatched-item .MyLabel.corner
 {
-  text-align: center!important;
+    text-align: center !important;
 }
 
 .episodes-by-season__season .MyLabel.corner > .MyLabel__wrapper,
 .Unwatched-item .MyLabel.corner > .MyLabel__wrapper
 {
-  border-radius: 2px!important;
-  background-color: var(--color-gray-170) !important;
+    border-radius: 2px !important;
+    background-color: var(--color-gray-170) !important;
 }
 
 .dark-mode .episodes-by-season__season .MyLabel.corner > .MyLabel__wrapper,
 .dark-mode .Unwatched-item .MyLabel.corner > .MyLabel__wrapper
 {
-  background-color: var(--comments-label-color) !important;
+    background-color: var(--comments-label-color) !important;
 }
 
 .episodes-by-season__season .MyLabel.corner > .MyLabel__wrapper > .MyLabel__corner,
 .Unwatched-item .MyLabel.corner > .MyLabel__wrapper > .MyLabel__corner
 {
-  color: transparent!important;
-  width: 0!important;
-  border: unset!important;
+    color: transparent !important;
+    width: 0 !important;
+    border: unset !important;
 }
 
-.episode-col {
-  font-size: 14px!important;
+.episode-col
+{
+    font-size: 14px !important;
 }
 
-.UnwatchedEpisodeItem__index {
-  width: 16px!important;
-  display: flex!important;
-  justify-content: flex-end!important;
+.UnwatchedEpisodeItem__index
+{
+    width: 16px !important;
+    display: flex !important;
+    justify-content: flex-end !important;
 }
 
-.collapseAll {
-    display: block;
+.collapseAll
+{
+    display: block !important;
+    cursor: pointer !important;
+    right: 22.7rem !important;
+    position: absolute !important;
+    color: var(--info-color) !important;
+    user-select: none !important;
+}
+
+a.episode-col__label:hover
+{
+    text-decoration: none !important;
+}
+
+.TopNavigation__wrapper
+{
+    gap: 5px !important;
+}
+
+.TopNavigation__wrapper button.TopNavigation__link
+{
+    font-size: 15px !important;
+    line-height: 20px !important;
+    padding: 5px 8px !important;
+}
+
+.Row-container .Col
+{
+    padding: 4px 0 !important;
+}
+
+.episodes-by-season__season .Row-container
+{
+    padding-left: 20px !important;
+}
+
+@media (hover: hover) and (pointer: fine)
+{
+    .HeaderMenu__item:not(.HeaderMenu__item--disabled):hover .HeaderMenu__dropdown,
+    .HeaderMenu__item:not(.HeaderMenu__item--disabled):hover .HeaderMenu__link-wrapper:after
+    {
+        opacity: 0 !important;
+        visibility: hidden !important;
+    }
+}
+
+.dropDownButton
+{
+    user-select: none;
     cursor: pointer;
-    right: 22.7rem;
-    position: absolute;
-    color: var(--info-color);
+    padding-left: 0.3em;
 }
 
-a.episode-col__label:hover {
-    text-decoration: none;
+.dropDownButton:hover
+{
+    color: var(--header-link-hover-color);
 }
 
-.TopNavigation__wrapper {
-    gap: 5px;
-}
-
-.TopNavigation__wrapper button.TopNavigation__link {
-    font-size: 15px;
-    line-height: 20px;
-    padding: 5px 8px;
-}
-
-.Row-container .Col {
-    padding: 4px 0;
-}
-
-.episodes-by-season__season .Row-container {
-    padding-left: 20px;
+.dropDownButton[pressed="true"]
+{
+    color: var(--myshows-color);
 }
 `);
 
@@ -199,6 +239,56 @@ a.episode-col__label:hover {
         });
     }
 
+    function HeaderMenuButtonsFix()
+    {
+        function HideAll(excludedNode)
+        {
+            document.querySelectorAll('.HeaderMenu__item .dropDownButton').forEach(dropDownButton =>
+            {
+                if (excludedNode === dropDownButton) return;
+                if (dropDownButton.getAttribute('pressed') === 'true') dropDownButton.onclick();
+            });
+        }
+
+        document.querySelectorAll('.LayoutWrapper__background, .LayoutWrapper__main').forEach(node =>
+        {
+            node.addEventListener('mousedown', () => HideAll());
+        });
+
+        document.querySelectorAll('.HeaderMenu__item').forEach(item =>
+        {
+            let wrapper = item.querySelector('.HeaderMenu__link-wrapper');
+
+            if (wrapper.textContent !== 'Сериалы' && wrapper.textContent !== 'Фильмы') return;
+
+            if (item.querySelector('.dropDownButton')) return;
+
+            let dropdown = item.querySelector('.HeaderMenu__dropdown');
+
+            let dropDownButton = document.createElement('div');
+            dropDownButton.classList.add('dropDownButton');
+            dropDownButton.textContent = '▾';
+            dropDownButton.onclick = () =>
+            {
+                HideAll(dropDownButton);
+
+                if (dropDownButton.getAttribute('pressed') === 'true')
+                {
+                    dropdown.style.setProperty('opacity', '0', 'important');
+                    dropdown.style.setProperty('visibility', 'hidden', 'important');
+                    dropDownButton.setAttribute('pressed', 'false');
+                }
+                else
+                {
+                    dropdown.style.setProperty('opacity', '1', 'important');
+                    dropdown.style.setProperty('visibility', 'visible', 'important');
+                    dropDownButton.setAttribute('pressed', 'true');
+                }
+            };
+            wrapper.append(dropDownButton);
+        });
+    }
+
 
     function RunAfterDOMLoaded()
     {
@@ -207,6 +297,7 @@ a.episode-col__label:hover {
             CorrectionOfGrammaticalErrors();
             AddCollapseAllButton();
             FixWidthCommentButtons();
+            HeaderMenuButtonsFix();
         });
 
         observer.observe(document.body,
